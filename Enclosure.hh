0ce03aa444333248683f1ac1d4d26a6542482f51
Ceedrich <cedric@lehr.ch>
2024-03-21 10:49:58 +0100

    formatted Enclosure

--- Enclosure.hh
+++ Enclosure.hh
@@ -11,7 +11,7 @@ private:
 public:
   /**
    * @brief Constructs an Enclosure object with the given dimensions.
-   * 
+   *
    * @param w The width of the enclosure.
    * @param l The length of the enclosure.
    * @param h The height of the enclosure.
@@ -20,7 +20,7 @@ public:
 
   /**
    * @brief Constructs a square Enclosure object with the given side length.
-   * 
+   *
    * @param l The side length of the square enclosure.
    */
   Enclosure(double l) : Enclosure(l, l, l) {}
@@ -32,28 +32,28 @@ public:
 
   /**
    * @brief Returns the length of the enclosure.
-   * 
+   *
    * @return The length of the enclosure.
    */
   double length() const { return length_; }
 
   /**
    * @brief Returns the width of the enclosure.
-   * 
+   *
    * @return The width of the enclosure.
    */
   double width() const { return width_; }
 
   /**
    * @brief Returns the height of the enclosure.
-   * 
+   *
    * @return The height of the enclosure.
    */
   double height() const { return height_; }
 
   /**
    * @brief Draws the enclosure on the given DrawingFrame.
-   * 
+   *
    * @param support The DrawingFrame to draw on.
    */
   virtual void draw_on(DrawingFrame &support) override { support.draw(*this); }
@@ -61,7 +61,7 @@ public:
 
 /**
  * @brief Overloads the << operator to allow printing of Enclosure objects.
- * 
+ *
  * @param stream The output stream.
  * @param enclosure The Enclosure object to print.
  * @return The modified output stream.
