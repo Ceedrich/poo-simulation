ec0c4c34cd03ef273c70aa9766e647506d3243af
Ceedrich <cedric@lehr.ch>
2024-03-21 10:48:45 +0100

    made  inherit from

--- Particle.hh
+++ Particle.hh
@@ -1,17 +1,21 @@
 #pragma once
 
+#include "Drawable.hh"
 #include "Vector3D.hh"
 #include <iostream>
 
-class Particle
-{
+class Particle : public Drawable {
 public:
-    Particle(Vector3D pos, Vector3D v, double m) : position(pos), speed(v), mass(m) {}
-    void print(std::ostream& out) const;
+  Particle(Vector3D pos, Vector3D v, double m)
+      : position(pos), speed(v), mass(m) {}
+  virtual void print(std::ostream &out) const;
+
+  virtual void draw_on(DrawingFrame &support) override { support.draw(*this); }
+
 private:
-    Vector3D position;
-    Vector3D speed;
-    double mass;
+  Vector3D position;
+  Vector3D speed;
+  double mass;
 };
 
-std::ostream& operator<<(std::ostream& out, Particle element);
+std::ostream &operator<<(std::ostream &out, Particle const &element);
