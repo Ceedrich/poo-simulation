906e7ae19fcc08aaa00e493c7d4c5e07f23f4faf
Ceedrich <cedric@lehr.ch>
2024-04-06 19:53:51 +0200

    made all tests compatible with the new `System` and `Particle` classes

--- test_System.cc
+++ test_System.cc
@@ -10,18 +10,15 @@ using namespace std;
 int main() {
   System system1;
   System system2(5.0, 2.3, 1.0);
+  system1.add_particle(Helium(Vector3D(1, 1, 1), Vector3D(), 4.002602));
   system1.add_particle(
-      std::make_unique<Helium>(Vector3D(1, 1, 1), Vector3D(), 4.002602));
-  system1.add_particle(std::make_unique<Neon>(Vector3D(1, 18.5, 1),
-                                              Vector3D(0, 0.2, 0), 20.1797));
-  system1.add_particle(std::make_unique<Argon>(Vector3D(1, 1, 3.1),
-                                               Vector3D(0, 0, -0.5), 39.948));
+      Neon(Vector3D(1, 18.5, 1), Vector3D(0, 0.2, 0), 20.1797));
+  system1.add_particle(
+      Argon(Vector3D(1, 1, 3.1), Vector3D(0, 0, -0.5), 39.948));
 
-  std::unique_ptr<Particle> neon =
-      std::make_unique<Neon>(Vector3D(), Vector3D(), 5);
+  Neon neon(Vector3D(), Vector3D(), 5);
 
-  system2.add_particle(std::make_unique<Particle>(Vector3D(), Vector3D(), 123));
-  system2.add_particle(std::move(neon));
+  system2.add_particle(neon);
 
   cout << system1 << endl;
   cout << system2 << endl;
