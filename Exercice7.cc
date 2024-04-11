cc0b013683796a1530b1634a69352ec3f65214c7
Ceedrich <cedric@lehr.ch>
2024-04-11 15:02:23 +0200

    Update CmakeLists.txt

--- Exercice7.cc
+++ Exercice7.cc
@@ -1,5 +1,6 @@
 #include "Enclosure.hh"
 #include "Particles/Argon.hh"
+#include "Particles/Helium.hh"
 #include "Particles/Neon.hh"
 #include "Text/TextViewer.hh"
 #include "Vector3D.hh"
@@ -9,6 +10,7 @@ int main() {
   TextViewer textViewer;
   Neon neon(Vector3D(1, 2, 3), Vector3D(), 3.12);
   Argon argon(Vector3D(1, 0, 0), Vector3D(0, 0, 1), 4.4);
+  Helium helium(Vector3D(12, 3, 9), Vector3D(0, -1, 0), 22.1);
 
   Enclosure e;
 
